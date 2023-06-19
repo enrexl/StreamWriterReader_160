d79db7f2fec4597e6f201385fe980d4d972f1f24
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-06-19 15:02:01 +0700

    Menambah perintah membaca file ketika file tidak tersedia

--- StreamWriterReader_160/Iofile.cpp
+++ StreamWriterReader_160/Iofile.cpp
@@ -36,7 +36,13 @@ int main(){
 	if (infile.is_open()) {
 		//melakukkan perulangan setiap baris
 		while (getline(infile, baris)) {
-
+			//dan tampilkan di sini
+			cout << baris << '\n';
 		}
+		//tutup file tersebut setelah selesai
+		infile.close();
 	}
+	//jika tidak ditemukan file maka akan menampilakan hal tersebut
+	else cout << "Unable to open file!";
+	return 0;
 }
