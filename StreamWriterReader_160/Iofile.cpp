8bdf025f91391dec281609524d7d3c10da0609da
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-06-19 14:57:11 +0700

    Memasukkan perintah membuka file

--- StreamWriterReader_160/Iofile.cpp
+++ StreamWriterReader_160/Iofile.cpp
@@ -20,5 +20,23 @@ int main(){
 		getline(cin, baris);
 		//loop akan berhenti jika anda memasukan karakter q
 		if (baris == "q") break;
+		//menulis dan memasukkan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
+
+	//membuka file dalam mode membaca
+	ifstream infile;
+	//menunjuk ke sebuah file
+	infile.open("contohfile.txt.");
+
+	cout << endl << ">= Membuka dan Membaca file " << endl;
+	//jika file tersedia, maka
+	if (infile.is_open()) {
+		//melakukkan perulangan setiap baris
+		while (getline(infile, baris)) {
+
+		}
 	}
 }
