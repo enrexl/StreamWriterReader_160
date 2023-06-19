46e0fda86cf1e1590415692810ae8062fea3bfa6
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-06-19 15:45:09 +0700

    menambah blok catch dan penulisan cout terakhir

--- ExceptionStandart/ExceptionStandart.cpp
+++ ExceptionStandart/ExceptionStandart.cpp
@@ -8,6 +8,16 @@ int main()
 	cout << "Awal Program" << endl;	//Penanda 1:...
 	try {
 		array<int, 3> data = { 1,2,3 }; // pesan arry integer 1 element
-		cout << data.at(3) << endl;		// Memanggil elemen array ke 3
+		cout << data.at(2) << endl;		// Memanggil elemen array ke 3
 	}
+	catch (exception& e) { //Penangkap object exception
+		cout << e.what() << endl;		//Eksekusi akan dilakukan jika kode try benar
+	}
+	
+		cout << "Baris Program yang Terakhir" << endl; 
+		/*Menandakan bahwa program berjalan sampai baris 
+		terakhir walau terdapat kesalahan*/
+	
 }
+
+
