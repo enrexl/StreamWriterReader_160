c3aee4e310525fe8f689436d43b7df539a1a249f
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-06-19 15:39:06 +0700

    Membuat balok try/ balok kode rawan kesalahan

--- ExceptionStandart/ExceptionStandart.cpp
+++ ExceptionStandart/ExceptionStandart.cpp
@@ -1,8 +1,13 @@
-
-
+#include<exception> //Library untuk object Exception
+#include <array> //Library objek array yang akan digunakan
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Awal Program" << endl;	//Penanda 1:...
+	try {
+		array<int, 3> data = { 1,2,3 }; // pesan arry integer 1 element
+		cout << data.at(3) << endl;		// Memanggil elemen array ke 3
+	}
 }
