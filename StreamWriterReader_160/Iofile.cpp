1f4d9c4a60ba05684762d7301a0deafed879678c
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-06-19 14:53:06 +0700

    Membuat penulisan file dan loop pembacaan baris

--- StreamWriterReader_160/Iofile.cpp
+++ StreamWriterReader_160/Iofile.cpp
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <fstream>
+#include<string>
 using namespace std;
 
-int main()
-{
-    
+int main(){
+	string baris;
+
+	// membuka file dalam mode menulis
+	ofstream outfile;
+	//menunjuk ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\q untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true) {
+		cout << "- ";
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+	}
 }
