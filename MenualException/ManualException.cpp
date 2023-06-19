26c29dfff20b295676eaa2e514427820e6327a66
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2023-06-19 15:30:00 +0700

    Membuat barisan kode yang akan rawan kesalahan

--- MenualException/ManualException.cpp
+++ MenualException/ManualException.cpp
@@ -1,7 +1,14 @@
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    try {
+        cout << "Selamat Belajar di Prodi TI UMY" << endl;
+        throw 0.5; //melemparkan sebuah integer, maka
+        cout << "Pernyataan tidak akan dieksekusi!" << endl;
+    }
+    catch{
+
+    }
 }
